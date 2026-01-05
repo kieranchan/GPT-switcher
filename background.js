32d0a401ff4d01c7ebe8619c992dff0348e800bd
Stefan <1119788227@qq.com>
2026-01-05 16:22:57 +0800

    chore: remove unused background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ currentAccount: null });
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({ url: 'popup.html' });
-});
